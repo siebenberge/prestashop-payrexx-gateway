7a625da984559cf7f6b29dc8ec032d4c6dbb67e8
vinothganesan <vinoth@payrexx.com>
2023-02-03 16:54:08 +0530

    task/PP-8332: added payment methods

--- payrexx/src/Util/ConfigurationUtil.php
+++ payrexx/src/Util/ConfigurationUtil.php
@@ -34,4 +34,50 @@ class ConfigurationUtil
             'PAYREXX_LOOK_AND_FEEL_ID',
         ];
     }
+
+    /**
+     * Get payment methods
+     *
+     * @return array
+     */
+    public static function getPaymentMethods(): array
+    {
+        return [
+            'masterpass' => 'Masterpass',
+            'mastercard' => 'Mastercard',
+            'visa' => 'Visa',
+            'apple_pay' => 'Apple Pay',
+            'maestro' => 'Maestro',
+            'jcb' => 'JCB',
+            'american_express' => 'American Express',
+            'wirpay' => 'WIRpay',
+            'paypal' => 'PayPal',
+            'bitcoin' => 'Bitcoin',
+            'sofortueberweisung_de' => 'Sofort Ueberweisung',
+            'airplus' => 'Airplus',
+            'billpay' => 'Billpay',
+            'bonuscard' => 'Bonus card',
+            'cashu' => 'CashU',
+            'cb' => 'Carte Bleue',
+            'diners_club' => 'Diners Club',
+            'direct_debit' => 'Direct Debit',
+            'discover' => 'Discover',
+            'elv' => 'ELV',
+            'ideal' => 'iDEAL',
+            'invoice' => 'Invoice',
+            'myone' => 'My One',
+            'paysafecard' => 'Paysafe Card',
+            'postfinance_card' => 'PostFinance Card',
+            'postfinance_efinance' => 'PostFinance E-Finance',
+            'swissbilling' => 'SwissBilling',
+            'twint' => 'TWINT',
+            'barzahlen' => 'Barzahlen/Viacash',
+            'bancontact' => 'Bancontact',
+            'giropay' => 'GiroPay',
+            'eps' => 'EPS',
+            'google_pay' => 'Google Pay',
+            'wechat_pay' => 'WeChat Pay',
+            'alipay' => 'Alipay',
+        ];
+    }
 }
