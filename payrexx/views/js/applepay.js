72b0479baca2db19f3c7518bd948c2351e16446e
vinoth <vinoth@payrexx.com>
2023-06-27 14:54:18 +0530

    task/PP-9377: code validation

--- payrexx/views/js/applepay.js
+++ payrexx/views/js/applepay.js
@@ -1,3 +1,10 @@
+/**
+ * Payrexx Payment Gateway.
+ *
+ * @author    Payrexx <integration@payrexx.com>
+ * @copyright 2023 Payrexx
+ * @license   MIT License
+ */
 $(document).on('ready', function() {
     if ((window.ApplePaySession && ApplePaySession.canMakePayments()) !== true) {
         var $containerId = $('input[name=payrexxPaymentMethod][value=apple-pay]')
