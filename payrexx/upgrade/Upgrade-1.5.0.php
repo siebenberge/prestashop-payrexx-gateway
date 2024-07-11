6b5b7bf1c4f121521c09503bb57941c01db480a5
vinoth <vinoth@payrexx.com>
2024-07-11 19:54:50 +0530

    task/PP-12178: improved query to set post finance pay active

--- payrexx/upgrade/Upgrade-1.5.0.php
+++ payrexx/upgrade/Upgrade-1.5.0.php
@@ -12,5 +12,26 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_1_5_0($module)
 {
-    return include _PS_MODULE_DIR_ . 'payrexx/sql/install.php';
+    $status = include _PS_MODULE_DIR_ . 'payrexx/sql/install.php';
+
+    $paymentMethodSql = new DbQuery();
+    $paymentMethodSql->select('pm')
+        ->from('payrexx_payment_methods')
+        ->where('active = 1')
+        ->where('pm = "post-finance-card" OR pm = "post-finance-e-finance"');
+    $results = Db::getInstance()->executeS($paymentMethodSql);
+
+    // activate post-finance-pay
+    if (!empty($results)) {
+        $updateSql = 'UPDATE `' . _DB_PREFIX_ . 'payrexx_payment_methods`
+            SET `active` = 1
+            WHERE  `pm` = "post-finance-pay"';
+        Db::getInstance()->execute($updateSql);
+    }
+
+    $deleteSql = 'DELETE FROM `' . _DB_PREFIX_ . 'payrexx_payment_methods`
+        WHERE  `pm` = "post-finance-card" OR `pm` = "post-finance-e-finance"';
+    Db::getInstance()->execute($deleteSql);
+
+    return $status;
 }
